101045cc0734c19a4148c07dfcfc01a8101a03cd
Renan Teixeira <rteixeira1997@gmail.com>
2025-07-19 20:55:17 -0300

    Lista Tarefas

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(blank=True)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    concluida = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        return reverse('tarefa_detail', kwargs={'pk': self.pk})
+    class Meta:
+        ordering = ['-data_criacao']
+        verbose_name = 'Tarefa'
+        verbose_name_plural = 'Tarefas'
